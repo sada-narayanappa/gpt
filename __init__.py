9d8852ce84c13d680f7ae65c074a1e94c41b84b7
sada-narayanappa <sada.narayanappa@gmail.com>
2023-03-13 08:33:43 -0600

    Commiting

--- __init__.py
+++ __init__.py
@@ -1,9 +1,16 @@
 import os
 
 print ("Initializing web services: " + os.getcwd())
-if (os.path.exists("chatgpt/chatgpt.py")):
+if (os.path.exists("gpt/chatgpt.py")):
     from . import chatgpt
 else:
     print("ChatGPT file does not exist! Were you expecting this?")
     
+
+if (os.path.exists("gpt/whisper.py")):
+    from . import whisper
+    print("whisper Services Loaded!")
+else:
+    print("whisper file does not exist! Were you expecting this?")
+
 
