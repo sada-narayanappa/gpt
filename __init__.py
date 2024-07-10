1949588b8111e1894c5b2f3b41f87e37ffd6b778
geospaces-org <geo1@geospaces.org>
2024-07-10 09:57:38 -0600

    Commiting

--- __init__.py
+++ __init__.py
@@ -1,15 +1,16 @@
-import os
+import os, sys
 
 print ("Initializing web services: " + os.getcwd())
 if (os.path.exists("gpt/chatgpt.py")):
-    from . import chatgpt
+    pass
+    #from . import chatgpt
 else:
     print("ChatGPT file does not exist! Were you expecting this?")
     
 
-if (os.path.exists("gpt/whisper.py")):
-    from . import whisper
-    print("whisper Services Loaded!")
+if (os.path.exists("gpt/whispermod.py")):
+    #from . import whispermod
+    print("whisper Services to be Loaded!")
 else:
     print("whisper file does not exist! Were you expecting this?")
 
